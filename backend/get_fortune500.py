b9c209883a7433f422d7e02e55b1e873f189684d
rpritr <robi@foreach-labs.si>
2024-10-23 17:34:20 +0200

    add data to api from fortune 500

--- backend/get_fortune500.py
+++ backend/get_fortune500.py
@@ -17,16 +17,30 @@ def get_fortune500_companies():
         for row in table_rows[:20]:  # Pridobimo prvih 20 podjetij
             cells = row.find_all('td')
             if len(cells) > 0:
-                rank = cells[0].get_text(strip=True)  # Rank (1, 2, 3,...)
+                rank = cells[0].get_text(strip=True)  # Rang (1, 2, 3,...)
                 name = cells[1].get_text(strip=True)  # Ime podjetja (Walmart,...)
                 revenue = cells[2].get_text(strip=True)  # Prihodki (Revenue)
+                revenue_change = cells[3].get_text(strip=True)  # Sprememba prihodkov v %
                 profit = cells[4].get_text(strip=True)  # Dobiček (Profit)
+                profit_change = cells[5].get_text(strip=True)  # Sprememba dobička v %
+                assets = cells[6].get_text(strip=True)  # Sredstva
+                employees = cells[7].get_text(strip=True)  # Stevilo zaposlenih
+                rank_change = cells[8].get_text(strip=True)  # Sprememba ranga
+                years_in_rank = cells[9].get_text(strip=True)  # Stevilo let v rangu
 
                 companies.append({
                     'rank': rank,
                     'name': name,
                     'revenue': revenue,
-                    'profit': profit
+                    'revenue_change': revenue_change,
+                    'profit': profit,
+                    'profit_change': profit_change,
+                    'assets': assets,
+                    'employees': employees,
+                    'rank_change': rank_change,
+                    'years_in_rank': years_in_rank,
+
+
                 })
 
         return companies
